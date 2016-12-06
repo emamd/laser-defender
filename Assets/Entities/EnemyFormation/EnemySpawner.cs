3026be733e38100f8a5c8ab965fb4a436ca830ea
Armand Emamdjomeh <armand.emamdjomeh@latimes.com>
2016-12-06 11:19:56 -0800

    starting git repo

---
+++ Assets/Entities/EnemyFormation/EnemySpawner.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+	public GameObject enemyPrefab;
+	public float width = 10.0f;
+	public float height = 5.0f;
+	public float moveSpeed = 7.0f; 
+
+	private bool movingRight = false;
+	private float xmin;
+	private float xmax;
+
+	// Use this for initialization
+	void Start () {
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0,0,distance));
+		Vector3 rightmost = Camera.main.ViewportToWorldPoint (new Vector3(1,0,distance));
+		xmin = leftmost.x;
+		xmax = rightmost.x;
+
+		foreach (Transform child in transform) { 
+			GameObject enemy = Instantiate (enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
+			enemy.transform.parent = child.transform; 	
+		}
+	}
+
+	void OnDrawGizmos() {
+		Gizmos.DrawWireCube (transform.position, new Vector3 (width, height, 0));
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (movingRight) {
+			transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+		} else {
+			transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+		}
+
+		float formationRightEdge = transform.position.x + width / 2;
+		float formationLeftEdge = transform.position.x - width / 2;
+
+		if (formationLeftEdge < xmin) {
+			movingRight = true;
+		} else if (formationRightEdge > xmax) {
+			movingRight = false;
+		}
+	}
+}
---
+++ Assets/Entities/Player/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed;
+	public float padding = 1.0f;
+	public GameObject projectile;
+	public float projectileSpeed;
+	public float fireRate;
+	float xmin;
+	float xmax;
+
+	void Start() {
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftmost = Camera.main.ViewportToWorldPoint (new Vector3(0,0,distance));
+		Vector3 rightmost = Camera.main.ViewportToWorldPoint (new Vector3(1,0,distance));
+		xmin = leftmost.x + padding;
+		xmax = rightmost.x - padding;
+	}
+
+	void Fire() {
+		GameObject beam = Instantiate (projectile, transform.position, Quaternion.identity) as GameObject;
+		beam.rigidbody2D.velocity = new Vector3(0, projectileSpeed, 0);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.RightArrow)) {
+ 			transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+		} else if (Input.GetKey(KeyCode.LeftArrow)) {
+			transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+		}
+
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			InvokeRepeating("Fire", 0.00001f, fireRate);
+		} else if (Input.GetKeyUp (KeyCode.Space)) {
+			CancelInvoke("Fire");
+		}
+
+		// restrict player to the game space. 
+		float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
+		transform.position = new Vector3 (newX, transform.position.y, transform.position.z);
+	}
+}
