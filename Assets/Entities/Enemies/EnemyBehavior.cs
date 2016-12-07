39363d1f607cdd7d4c1be501538bb0a6ee7d8c69
Armand Emamdjomeh <armand.emamdjomeh@latimes.com>
2016-12-06 21:59:35 -0800

    destroy things woooo

---
+++ Assets/Entities/Enemies/EnemyBehavior.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehavior : MonoBehaviour {
+	public float health = 150f;
+
+	void OnTriggerEnter2D(Collider2D col) {
+		Projectile projectile = col.gameObject.GetComponent<Projectile> ();
+		if (projectile) {
+			health -= projectile.GetDamage();
+
+			if (health <= 0.0) {
+				Debug.Log (health);
+				Destroy(gameObject);
+			}
+
+			projectile.Hit();
+
+		}
+	}
+
+
+}
